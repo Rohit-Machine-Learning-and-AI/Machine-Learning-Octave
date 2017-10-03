038b920104f77a85632792ee9aeaa71c1958fa7a
Rohit <rohitnair987@gmail.com>
2017-10-02 20:08:10 -0400

    multi variate linear regression

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -35,21 +35,23 @@ fprintf('Loading data ...\n');
 
 %% Load Data
 data = load('ex1data2.txt');
-X = data(:, 1:2);
+X1 = data(:, 1:2);
 y = data(:, 3);
 m = length(y);
 
 % Print out some data points
 fprintf('First 10 examples from the dataset: \n');
-fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+fprintf(' x = [%.0f %.0f], y = %.0f \n', [X1(1:10,:) y(1:10,:)]');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 % Scale features and set them to zero mean
 fprintf('Normalizing Features ...\n');
+%X1
+%fprintf('size: %d\n', size(X1));
 
-[X mu sigma] = featureNormalize(X);
+[X mu sigma] = featureNormalize(X1);
 
 % Add intercept term to X
 X = [ones(m, 1) X];
